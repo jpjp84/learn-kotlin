44b507719feb7631e5da8584e6851a59e6710516
jp <jpjp8414@gmail.com>
2020-03-05 12:06:27 +0900

    Practice Classes and Objects chapter (Lamdas#2 Lamda Expression & Anonymous Function) - https://www.notion.so/Lamdas-4d9544b0f6b24d55b338e445d4d32eee

--- src/com/jp/function_lamda/Lamdas.kt
+++ src/com/jp/function_lamda/Lamdas.kt
@@ -1,6 +1,14 @@
 package com.jp.function_lamda
 
 fun main() {
+    highOrderFunctionTest()
+    functionTypeTest()
+    instantiatingFunctionType()
+    lamdaExpressionTest()
+    receiverFunctionTest()
+}
+
+fun highOrderFunctionTest() {
     val arrays = arrayListOf("test1", "test2", "test3", "test4", "test5")
     arrays.fold(mutableListOf()) { acc: MutableList<String>, i: String ->
         println("acc = $acc, i = $i, ")
@@ -20,3 +28,89 @@ fun main() {
 //    }
 //    return accumulator
 //}
+
+class LamdasA {
+    fun showMessage(msg: String) {
+        println("message : $msg")
+    }
+}
+
+class LamdasB {
+    object LamdaBObject {
+        val data: String = "LamdaBObject"
+    }
+}
+
+object LamdasC {
+    val data: String = "test"
+}
+
+fun functionTypeTest() {
+    println()
+    // function type 정의
+    val onClick: (LamdasA, LamdasB) -> String
+    val onMove: LamdasB.(LamdasB.LamdaBObject) -> String
+
+    val getOnClick: ((LamdasA, LamdasB) -> String) -> Unit
+
+    // instantiating function type
+    val showMessage: LamdasA.(String) -> Unit = { msg ->
+        this.showMessage(msg)
+    }
+    val twoParamShowMessage: (LamdasA, String) -> Unit = showMessage
+
+    fun lamdaPrintln(f: (LamdasA, String) -> Unit) {
+        f(LamdasA(), "its Lamda A Message!")
+    }
+
+    lamdaPrintln(showMessage)
+}
+
+fun instantiatingFunctionType() {
+    println()
+    val stringPlus: (String, String) -> String = String::plus
+    val intPlus1: (Int, Int) -> Int = Int::plus
+    val intPlus2: Int.(Int) -> Int = Int::plus
+
+    println(stringPlus("test", " message"))
+    println(intPlus1.invoke(4, 5))
+    println(1.intPlus2(4))
+}
+
+fun lamdaExpressionTest() {
+    println()
+    val sum: (Int, Int) -> Int = { x: Int, y: Int -> x + y }
+    val sum2 = { x: Int, y: Int -> x + y }
+    //it의 사용
+//    val sumWith4: (Int) -> Int = { x: Int -> x + 4 }
+    val sumWith4: (Int) -> Int = { it + 4 }
+
+    val ints = arrayListOf(1, 2, 3, 4, 5)
+    val fileteredInt = ints.filter {
+        val result = it > 1
+        return@filter result
+    }
+
+    ints.filter(fun(item) = item > 0)
+
+    println(fileteredInt)
+}
+
+fun receiverFunctionTest() {
+    html {
+        // lambda with receiver begins here
+        body()   // calling a method on the receiver object
+    }
+}
+
+class HTML {
+    fun body() {
+
+    }
+}
+
+fun html(init: HTML.() -> Unit): HTML {
+    val html = HTML()  // create the receiver object
+    html.init()        // pass the receiver object to the lambda
+    return html
+}
