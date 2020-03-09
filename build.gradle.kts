456e464dc7540f0db8ad8b758288e548aeef432a
jp <jpjp8414@gmail.com>
2020-03-09 11:11:59 +0900

    set repository and project - gitignore 설정 및 gradle 설정

---
+++ build.gradle.kts
@@ -0,0 +1,24 @@
+plugins {
+    kotlin("jvm") version "1.3.70"
+}
+
+group = "learn-kotlin"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    implementation(kotlin("stdlib-jdk8"))
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.3.4")
+}
+
+tasks {
+    compileKotlin {
+        kotlinOptions.jvmTarget = "1.8"
+    }
+    compileTestKotlin {
+        kotlinOptions.jvmTarget = "1.8"
+    }
+}
---
+++ src/main/kotlin/com/jp/coroutines/Basic.kt
@@ -0,0 +1,14 @@
+package com.jp.coroutines
+
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.launch
+
+fun main() {
+    globalCoroutine()
+}
+
+fun globalCoroutine() {
+    GlobalScope.launch {
+
+    }
+}
