98895071289ab52b4dee5638dae2856bfda86cd1
jp <jpjp8414@gmail.com>
2020-05-19 13:56:54 +0900

    Leet Code - LongestCommonPrefix - binary tree 활용하여 수정

--- src/main/kotlin/com/jp/leetcode/LongestCommonPrefix.kt
+++ src/main/kotlin/com/jp/leetcode/LongestCommonPrefix.kt
@@ -2,33 +2,43 @@ package com.jp.leetcode
 
 
 fun main() {
-    val strs = mutableListOf("c", "c")
+    val strs = mutableListOf("aca", "cba")
+//    val strs = emptyList<String>()
     print("solution ${LongestCommonPrefix().longestCommonPrefix(strs.toTypedArray())}")
 }
 
 class LongestCommonPrefix() {
-    fun longestCommonPrefix(strs: Array<String>): String {
-        var index = 1
-        var pivotStr = ""
-        loop@ while (true) {
-            try {
-                val temp = strs[0].substring(0, index)
-                val filtered = strs
-                        .filterIndexed { strIndex, _ -> strIndex != 0 }
-                        .filter { it.length >= index }
-                        .filter { it.substring(0, index) == temp }
 
-                if (filtered.size != strs.size - 1) {
-                    break@loop
-                }
-                pivotStr = temp
-                index++
-            } catch (e: Exception) {
-                break@loop
-            }
+    fun longestCommonPrefix(strs: Array<String>): String {
+        if (strs.isEmpty()) {
+            return ""
+        }
+        return longestCommonPrefix(strs, 0, strs.size - 1)
+    }
+
+    fun longestCommonPrefix(strs: Array<String>, l: Int, r: Int): String {
+        if (l == r) {
+            return strs[l]
         }
 
-        return pivotStr
+        val mid = (l + r) / 2
+        val left = longestCommonPrefix(strs, l, mid)
+        val right = longestCommonPrefix(strs, mid + 1, r)
+
+        return compare(left, right)
     }
+
+    private fun compare(left: String, right: String): String {
+        var extractStr = ""
+        left.zip(right) { a, b ->
+            if (a != b) {
+                return extractStr
+            }
+            extractStr += a
+        }
+
+        return extractStr
+    }
+
 }
 
