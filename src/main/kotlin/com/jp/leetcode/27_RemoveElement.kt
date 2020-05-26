52a4d4922b9b0bf1ae0beec612a5a1ccb93e7cba
jp <jpjp8414@gmail.com>
2020-05-26 23:34:31 +0900

    Leet Code - 27. Remove Element - approach2 & refactoring

--- src/main/kotlin/com/jp/leetcode/27_RemoveElement.kt
+++ src/main/kotlin/com/jp/leetcode/27_RemoveElement.kt
@@ -8,25 +8,21 @@ import java.util.*
  */
 
 fun main() {
-    val nums = intArrayOf(3, 2, 2, 3)
-    val value = 3
+    val nums = intArrayOf(0,1,2,2,3,0,4,2)
+    val value = 2
     val result = RemoveElement().removeElement(nums, value)
     println("Solution : $result, ${nums.toList()}, ${nums.indexOf(value)}")
 }
 
 class RemoveElement() {
     fun removeElement(nums: IntArray, value: Int): Int {
-        var removedElementCount = 0
+        if (!nums.contains(value)) {
+            return nums.size
+        }
 
         nums.mapIndexed { index, i ->
-            if (i == value) {
-                return@mapIndexed
-            }
-
-            removedElementCount++
-
             val position = nums.indexOfFirst { it == value }
-            if (position < 0 || index < position) {
+            if (i == value || index < position) {
                 return@mapIndexed
             }
 
@@ -34,7 +30,7 @@ class RemoveElement() {
             nums[index] = value
         }
 
-        return removedElementCount
+        return nums.indexOf(value)
     }
 }
 
