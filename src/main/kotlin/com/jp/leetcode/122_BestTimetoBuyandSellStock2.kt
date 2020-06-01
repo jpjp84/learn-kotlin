6f672335cc85b2b1027f33f6f0c1653cb9077f46
jp <jpjp8414@gmail.com>
2020-06-01 14:31:32 +0900

    LeetCode - Best Time To Buy And Sell Stock2 - Approach2

--- src/main/kotlin/com/jp/leetcode/122_BestTimetoBuyandSellStock2.kt
+++ src/main/kotlin/com/jp/leetcode/122_BestTimetoBuyandSellStock2.kt
@@ -11,53 +11,15 @@ fun main() {
 }
 
 class BestTimetoBuyandSellStock2 {
+    fun maxProfit(prices: IntArray): Int {
+        var sum = 0
 
-    fun maxProfit2(prices: IntArray): Int {
-        //buy의 금액보다 남은 array에 큰 수가 있어야 한다.
-        //선택한 buy의 금액이 이전의 array에서 작은 수가 있으면 볼 필요가 없다.
-
-        //나올 수 있는 경우의 수들을 계산해 본다.
-        // (1, 5) (1, 3) (1, 6) (1, 4)  -> (1, 2) (1, 3) (1, 4) (1, 5)
-        // (5, 6)                       -> (2, 4)
-        // (3, 6) (3, 4)                -> (3, 4) (3, 5)
-
-        // (1, 2) (1, 3), (1, 5)
-        // (3, 5)
-        // (4, 5)
-
-        val positions: MutableList<Pair<Int, Int>> = mutableListOf()
-        prices.mapIndexed { buyIndex, price ->
-            prices.slice(buyIndex + 1 until prices.size).mapIndexed { sellIndex, i ->
-                if (i > price) {
-                    positions.add(Pair(buyIndex, buyIndex + sellIndex + 1))
-                }
+        for (i in prices.size - 2 downTo 0) {
+            if (prices[i + 1] > prices[i]) {
+                sum += prices[i + 1] - prices[i]
             }
         }
-
-        var index = 0
-        var result = 0
-        while (index != positions.size - 1) {
-            var lastSelect: Pair<Int, Int> = positions[index] ?: return 0
-            var sum = prices[lastSelect.second] - prices[lastSelect.first]
-
-            positions.mapIndexed { positionIndex, position ->
-                if (index >= positionIndex) {
-                    return@mapIndexed
-                }
-
-                if (lastSelect.first == position.first || lastSelect.second >= position.first) {
-                    return@mapIndexed
-                }
-                println("$lastSelect, $position")
-                lastSelect = position
-                sum += prices[lastSelect.second] - prices[lastSelect.first]
-            }
-            index++
-            result = Math.max(sum, result)
-            println("---------")
-        }
-
-        return result
+        return sum
     }
 }
 
