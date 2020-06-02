823cf4959da1266ad5237a044a7386411d0f7e99
jp <jpjp8414@gmail.com>
2020-06-02 12:26:37 +0900

    Programmers - MakeBitInt - Approach2. use while

--- src/main/kotlin/com/jp/programmers/greedy/MakeBigInt.kt
+++ src/main/kotlin/com/jp/programmers/greedy/MakeBigInt.kt
@@ -1,15 +1,13 @@
 package com.jp.programmers.greedy
 
-import java.util.*
-
 
 /**
  * https://www.notion.so/Remove-Element-cd10ee8700444901af87aaf33220c45a
  */
 
 fun main() {
-    val number = "5421345"
-    val k = 4
+    val number = "99991"
+    val k = 3
 
     //앞에서 부터 차례대로 돌다가 이전보다 큰 수가 나오면 이전 수를 지워준다
     println("Solution : ${MakeBigInt().solution(number, k)}")
@@ -18,31 +16,34 @@ fun main() {
 class MakeBigInt {
 
     fun solution(number: String, k: Int): String {
+        var answer = ""
         var removeCount = 0
-        val stack = Stack<Int>()
+        var index = 0
 
-        number.toList().map {
-            val currentNumber = Character.getNumericValue(it)
-            if (stack.isEmpty() || removeCount >= k) {
-                stack.push(currentNumber)
-                return@map
+        while (index < number.length) {
+            if (removeCount >= k) {
+                answer += number.substring(index)
+                break
             }
 
-            while (!stack.isEmpty()) {
-                val topStackNumber = stack.peek()
-                if (currentNumber <= topStackNumber || removeCount >= k) break
+            val currentNumber = Character.getNumericValue(number[index++])
+            if (answer.isEmpty()) {
+                answer += currentNumber
+                continue
+            }
 
-                stack.pop()
+            while (answer.isNotEmpty()) {
+                val lastNumber = Character.getNumericValue(answer[answer.length - 1])
+                if (currentNumber <= lastNumber || removeCount >= k) break
+
+                answer = answer.dropLast(1)
                 removeCount++
             }
 
-            stack.push(currentNumber)
+            answer += currentNumber
         }
 
-        for (i in 0 until k - removeCount) {
-            stack.pop()
-        }
-
-        return stack.fold("") { acc, stackNumber -> acc + stackNumber }
+        repeat(k - removeCount) { answer = answer.dropLast(1) }
+        return answer
     }
 }
