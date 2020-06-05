bc34b1160ea878cab15d39a1926a77ffce36084b
jp <jpjp8414@gmail.com>
2020-06-05 10:12:44 +0900

    Programmers - KNumber - Approach1

---
+++ src/main/kotlin/com/jp/programmers/align/KNumber.kt
@@ -0,0 +1,18 @@
+package com.jp.programmers.align
+
+fun main() {
+    val array = intArrayOf(1, 5, 2, 6, 3, 7, 4)
+    val command = arrayOf(
+            intArrayOf(2, 5, 3),
+            intArrayOf(4, 4, 1),
+            intArrayOf(1, 7, 3)
+    )
+    println("Solution : ${KNumber().solution(array, command)}")
+}
+
+class KNumber {
+    fun solution(array: IntArray, commands: Array<IntArray>): IntArray {
+        return commands.map { array.slice(it[0] - 1 until it[1]).sorted()[it[2] - 1] }.toIntArray()
+    }
+}
+
