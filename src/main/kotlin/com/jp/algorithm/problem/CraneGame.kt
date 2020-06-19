7a395ce3d0dbcc08d68b21ac3824f015ee78d60e
jp <jpjp8414@gmail.com>
2020-06-19 14:05:12 +0900

    Stack - P. CraneGame - Approach1.refactoring

--- src/main/kotlin/com/jp/algorithm/problem/CraneGame.kt
+++ src/main/kotlin/com/jp/algorithm/problem/CraneGame.kt
@@ -13,10 +13,10 @@ fun main() {
 
     val nodeinfo = arrayOf(
             intArrayOf(0, 0, 0, 0, 0),
-            intArrayOf(0, 0, 0, 0, 0),
-            intArrayOf(0, 0, 0, 0, 0),
-            intArrayOf(0, 0, 0, 0, 0),
-            intArrayOf(0, 0, 0, 0, 0)
+            intArrayOf(0, 0, 1, 0, 3),
+            intArrayOf(0, 2, 5, 0, 1),
+            intArrayOf(4, 2, 4, 4, 2),
+            intArrayOf(3, 5, 1, 3, 1)
     )
     val moves = intArrayOf(1, 5, 3, 5, 1, 2, 1, 4)
 
@@ -27,16 +27,12 @@ class CraneGame {
     fun solution(board: Array<IntArray>, moves: IntArray): Int {
         var answer = 0
         val stack = Stack<Int>()
-        val visit = Array(board.size) { 0 }.apply {
-            board.map { list ->
-                list.withIndex().filter { it.value == 0 }.forEach { this[it.index]++ }
-            }
-        }
+        val visit = Array(board.size) { visitIndex -> board.count { it[visitIndex] == 0 } }
+
         moves.map {
             if (visit[it - 1] >= board.size) {
                 return@map
             }
-
             val pick = board[visit[it - 1]][it - 1]
             if (stack.isNotEmpty() && stack.peek() == pick) {
                 stack.pop()
@@ -44,7 +40,7 @@ class CraneGame {
             } else {
                 stack.push(pick)
             }
-            visit[it - 1]++
+            visit[it - 1] += 1
         }
 
         return answer
