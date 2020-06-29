55df068665ffa922bce373b7cc359f9aa7835737
jp <jpjp8414@gmail.com>
2020-06-30 08:16:41 +0900

    Bitwise, Backtracking - L.22 Generate Parentheses approach1

---
+++ src/main/kotlin/com/jp/algorithm/problem/GenerateParentheses.kt
@@ -0,0 +1,48 @@
+package com.jp.algorithm.problem
+
+import java.util.*
+
+/**
+ * https://www.notion.so/Generate-Parentheses-610eb0b10f8544fd9b7ecfaa65574390
+ */
+
+fun main() {
+    println("Solution : ${GenerateParentheses().generateParenthesis(1)}")
+}
+
+class GenerateParentheses {
+    fun generateParenthesis(n: Int): List<String> {
+        val answer = mutableListOf<String>()
+        for (i in 0..1.shl(n * 2)) {
+            val binaryString = Integer.toBinaryString(i)
+
+            if (binaryString[0] != '1' || binaryString.length != n * 2) {
+                continue
+            }
+            val count0 = binaryString.count { it == '0' }
+            val count1 = binaryString.count { it == '1' }
+            if (count0 != count1 || !isMatchParenthesis(binaryString)) {
+                continue
+            }
+            answer.add(binaryString.replace("""[1]""".toRegex(), "(").replace("""[0]""".toRegex(), ")"))
+        }
+        return answer
+    }
+
+    private fun isMatchParenthesis(binaryString: String): Boolean {
+        val stack = Stack<Char>()
+        binaryString.forEach {
+            if (it == '1') {
+                stack.push(it)
+                return@forEach
+            }
+
+            if (stack.empty()) {
+                return false
+            }
+            stack.pop()
+        }
+
+        return true
+    }
+}
