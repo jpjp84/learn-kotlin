6396c39077fabe9a38149d52751707a3b7bd3668
jp <jpjp8414@gmail.com>
2020-07-06 13:15:30 +0900

    Greedy - P ShoppingGem approach2. 효율성 수정

--- src/main/kotlin/com/jp/algorithm/ShoppingGem.kt
+++ src/main/kotlin/com/jp/algorithm/ShoppingGem.kt
@@ -6,7 +6,7 @@ package com.jp.algorithm
 
 fun main() {
 
-    val gems = arrayOf("DIA", "RUBY", "RUBY", "DIA", "DIA", "EMERALD", "SAPPHIRE", "DIA")
+    val gems = arrayOf("DIA", "RUBY", "RUBY", "DIA", "DIA", "EMERALD", "SAPPHIRE", "DIA", "RUBY", "DIA", "DIA", "SAPPHIRE")
 //    val gems = arrayOf("AA", "AB", "AC", "AA", "AC")
 //    val gems = arrayOf("ZZZ", "YYY", "NNNN", "YYY", "BBB")
     println("Solution : ${ShoppingGem().solution(gems).toList()}")
@@ -14,6 +14,25 @@ fun main() {
 
 class ShoppingGem {
     fun solution(gems: Array<String>): IntArray {
+        val answer = intArrayOf(1, gems.size)
+        val tokenKindSize = gems.toSet().size
+        gems.foldIndexed(mutableMapOf<String, Int>()) { index, gemMap, gem ->
+            gemMap[gem] = index
+            if (gemMap.size == tokenKindSize) {
+                val start = gemMap.minBy { it.value }!!.value
+                val end = gemMap.maxBy { it.value }!!.value
+                if (end - start < answer[1] - answer[0]) {
+                    answer[0] = start + 1
+                    answer[1] = end + 1
+                }
+            }
+            gemMap
+        }
+
+        return answer
+    }
+
+    fun solution2(gems: Array<String>): IntArray {
         val answer = IntArray(2)
         val tokenKindSize = gems.toSet().size
         var tokenSize = tokenKindSize
